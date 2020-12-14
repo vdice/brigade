de0248fcecd65483d66d9495fc2907cf0e4739d9
Vaughn Dice <vadice@microsoft.com>
2020-12-14 11:38:11 -0700

    minor vcs-sidecar test script updates
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- v2/vcs-sidecar/test/test.sh
+++ v2/vcs-sidecar/test/test.sh
@@ -16,7 +16,7 @@ check_equal() {
 }
 
 # TODO: remove or update; we've opted to supply GitHub URLs directly in the test_clone method for now...
-# # areese mentioned updating this to init and create objects in a new local repo (create/push branches, tags, etc.)
+# # areese mentioned updating to serve a fresh, local repo and then creating objects (create/push branches, tags, etc.)
 # # Then, we won't have to clone an external repo
 setup_git_server() {
   local srvroot="${root_dir}/tmp"
@@ -39,14 +39,16 @@ setup_git_server() {
 }
 
 cleanup() {
-  pkill -9 git-daemon >/dev/null 2>&1
-  rm -rf "${tempdir}"
+  # These were from the deactivated local git server
+  # pkill -9 git-daemon >/dev/null 2>&1
+  # rm -rf "${tempdir}"
+  rm "${root_dir}/event.json"
 }
-# trap 'cleanup' EXIT
+trap 'cleanup' EXIT
 
 test_clone() {
   local revision="$1" want="$2"
-  local eventjson="${root_dir}/tmp/event.json"
+  local eventjson="${root_dir}/event.json"
 
   local key="ref"
   if [[ ${#revision} == 40 ]]; then
