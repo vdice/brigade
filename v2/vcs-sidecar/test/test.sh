ac0272b1627aab06e952447d233d591165a604ff
Vaughn Dice <vadice@microsoft.com>
2020-12-15 12:54:24 -0700

    clean up test script a bit
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- v2/vcs-sidecar/test/test.sh
+++ v2/vcs-sidecar/test/test.sh
@@ -15,45 +15,20 @@ check_equal() {
   }
 }
 
-# TODO: remove or update; we've opted to supply GitHub URLs directly in the test_clone method for now...
-# # areese mentioned updating to serve a fresh, local repo and then creating objects (create/push branches, tags, etc.)
-# # Then, we won't have to clone an external repo
-setup_git_server() {
-  local srvroot="${root_dir}/tmp"
-
-  git ls-remote git://localhost/test.git >/dev/null 2>&1 || git daemon --base-path="${srvroot}" --export-all --reuseaddr "${srvroot}" >/dev/null 2>&1 &
-
-  sleep 5
-
-  [[ -d  "${srvroot}" ]] && return
-
-  (
-  unset XDG_CONFIG_HOME
-  export HOME=/dev/null
-  export GIT_CONFIG_NOSYSTEM=1
-
-  repo_root="${root_dir}/tmp/test.git"
-
-  git clone --mirror https://github.com/brigadecore/empty-testbed.git "${repo_root}"
-  )
-}
-
 cleanup() {
-  # These were from the deactivated local git server
-  # pkill -9 git-daemon >/dev/null 2>&1
-  # rm -rf "${tempdir}"
-  rm "${root_dir}/event.json"
+  rm "${tempdir}/event.json"
 }
 trap 'cleanup' EXIT
 
 test_clone() {
   local revision="$1" want="$2"
-  local eventjson="${root_dir}/event.json"
+  local eventjson="${tempdir}/event.json"
 
   local key="ref"
   if [[ ${#revision} == 40 ]]; then
     key="commit"
   elif [[ ${#revision} == 0 ]]; then
+    # switch key to something bogus so that no ref or commit is included
     key="foo"
   fi
 
@@ -66,8 +41,7 @@ test_clone() {
 
   ../../bin/vcs-sidecar \
     -p "${eventjson}" \
-    -w "${BRIGADE_WORKSPACE}" # \
-    # --sshKey ./testkey.pem
+    -w "${BRIGADE_WORKSPACE}"
 
   got="$(git -C ${BRIGADE_WORKSPACE} rev-parse --short FETCH_HEAD)"
 
@@ -75,8 +49,6 @@ test_clone() {
 
   rm -rf "${BRIGADE_WORKSPACE}"
 }
-
-# setup_git_server
 
 # TODO: add fail cases (at least no ref found)
 
